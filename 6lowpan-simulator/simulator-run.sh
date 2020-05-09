6ff341a6a8be42e7079c7054453bc87f68b17958
Yara <57309798+yara0@users.noreply.github.com>
2020-05-09 01:31:19 -0400

    Create simulator-run.sh

---
+++ 6lowpan-simulator/simulator-run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 6LoWPAN Simulator
+# Author: Yara Altehini
+
+#install prerequisites
+sudo ./requirements.sh > log/requirementslog.txt 2>&1
+
+#run the simulator
+
+figlet -f big 6LoWPAN Simulator
+echo $'--------------- Yara Altehini ---------------\n\n'
+
+if [ $# -lt 1 ]; 
+	then 
+	echo "The number of devices was not specified";
+	exit 0
+fi
+
+sudo ./cleanup-docker-lowpan.sh $1 > log/cleanuplog.txt 2>&1
+sudo ./docker-lowpan.sh $1 > log/docker-lowpanlog.txt 2>&1
+
+
+#check if the simulator is running without any problems
+
+RESULT="$(sudo docker container exec iot0 ifconfig -a | grep 'wpan0')"
+
+if [ -z "$RESULT" ]
+then
+      echo $' The simulator failed to run. \n Please run the simulator again.'
+else
+      echo $' The simulator ran successfully. \n'
+fi
+
+
