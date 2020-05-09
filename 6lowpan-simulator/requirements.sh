e6e396b696966b843b0ab5a96196a65c8acd54b1
Yara <57309798+yara0@users.noreply.github.com>
2020-05-09 01:30:36 -0400

    Create requirements.sh

---
+++ 6lowpan-simulator/requirements.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 6LoWPAN Simulator
+# Author: Yara Altehini
+
+sudo apt-get update &&
+  apt-get install -y net-tools &&
+  apt-get install -y wpan-tools &&
+  apt-get install -y iputils-ping &&
+  apt-get install -y sed &&
+  apt-get install -y grep &&
+  apt install -y iproute2 &&
+  apt install -y netcat &&
+  apt install -y openssl &&
+  apt install -y figlet 
